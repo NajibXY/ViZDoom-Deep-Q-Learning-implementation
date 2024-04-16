d8ebd2b0902b11875106f3ff14f7d42370927dec
NajibXY <naj.elkhadir@gmail.com>
2024-04-16 20:35:30 +0200

    QNN base model

--- dql.py
+++ dql.py
@@ -1,12 +1,20 @@
-import numpy as np
-from random import sample
-
-import torch
 from vizdoom import *
 
-torch_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-resolution = (30, 45)
+import numpy as np
+from random import randint, random, sample
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from absl import flags
+from skimage.transform import resize
+
 config_path = "basic.cfg"
+torch_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Replay Memory
+
+resolution = (30, 45)
 
 class ReplayMemory:
     def __init__(self, replay_capacity):
@@ -36,3 +44,49 @@ class ReplayMemory:
     def sample(self, size):
         ind = sample(range(0, self.size), size)
         return (self.state1[ind], self.act[ind], self.state2[ind], self.isfinal[ind], self.rew[ind])
+
+# DNN Model
+FLAGS = flags.FLAGS
+class QNN(nn.Module):
+    def __init__(self, available_actions_count):
+        super(QNN, self).__init__()
+        # Layers
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=6, stride=3) 
+        self.conv2 = nn.Conv2d(8, 8, kernel_size=3, stride=2) 
+        self.fc1 = nn.Linear(192, 128)
+        self.fc2 = nn.Linear(128, available_actions_count)
+
+        self.criterion = nn.MSELoss()
+        self.optimizer = torch.optim.SGD(self.parameters(), FLAGS.learning_rate)
+        self.memory = ReplayMemory(capacity=FLAGS.replay_memory)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(-1, 192)
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+
+    def get_best_action(self, state):
+        q = self(state)
+        _, index = torch.max(q, 1)
+        return index
+
+    def train_step(self, state1, target_q):
+        output = self(state1)
+        loss = self.criterion(output, target_q)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        return loss
+
+    def learn_from_memory(self):
+        if self.memory.size < FLAGS.batch_size: return
+        state1, act, state2, isfinal, r = self.memory.sample(FLAGS.batch_size)
+        q = self(state2).detach()
+        q2, _ = torch.max(q, dim=1)
+        target_q = self(state1).detach()
+        idxs = (torch.arange(target_q.shape[0]), act)
+        target_q[idxs] = r + FLAGS.discount * (1-isfinal) * q2
+        self.train_step(state1, target_q)
+
